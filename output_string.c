14750438fb581d76158c947291f1f7fa88de4965
Niko Gontjarow <niko.gontjarow@gmail.com>
2020-08-06 20:36:18 +0300

    Norminette, string output

--- output_string.c
+++ output_string.c
@@ -1,81 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   output_string.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngontjar <ngontjar@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/06 19:06:59 by ngontjar          #+#    #+#             */
+/*   Updated: 2020/08/06 19:07:00 by ngontjar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-static void justify_left(char *arg, t_data *flag)
+static void	init(size_t *len, int *w, t_data *flag)
 {
-	// printf("\n");
+	if (flag->precision == -1)
+	{
+		flag->p = *len;
+	}
+	else
+	{
+		flag->p = flag->precision;
+	}
+	if (flag->p > *len)
+	{
+		flag->p = *len;
+	}
+	if ((size_t)flag->width > flag->p)
+	{
+		*w = (size_t)flag->width - flag->p;
+	}
+	else
+	{
+		*w = 0;
+	}
+}
 
+static void	justify_left(char *arg, t_data *flag)
+{
 	int		w;
 	size_t	len;
 
 	len = ft_strlen(arg);
-	// printf("len = %ld\n", len);
-
-	if (flag->precision == -1)
-		flag->p = len;
-	else
-		flag->p = flag->precision;
-	// printf("flag->p = %ld\n", flag->p);
-
-	if (flag->p > len)
-		flag->p = len;
-	// printf("flag->p = %ld\n", flag->p);
-
-	if ((size_t)flag->width > flag->p)
-		w = (size_t)flag->width - flag->p;
-	else
-		w = 0;
-	// printf("w = %ld\n", w);
-
+	init(len, w, flag);
 	flag->written += ft_putstrn(arg, flag->p);
-
-	while (w > 0)
-	{
-		write(1, " ", 1);
-		++flag->written;
-		--w;
-	}
+	width_padder(w, ' ', flag);
 }
 
-static void justify_right(char *arg, t_data *flag)
+static void	justify_right(char *arg, t_data *flag)
 {
-	// printf("\n");
-
 	int		w;
 	size_t	len;
 
 	len = ft_strlen(arg);
-	// printf("len = %ld\n", len);
-
-	if (flag->precision == -1)
-		flag->p = len;
-	else
-		flag->p = flag->precision;
-	// printf("flag->p = %ld\n", flag->p);
-
-	if (flag->p > len)
-		flag->p = len;
-	// printf("flag->p = %ld\n", flag->p);
-
-	if ((size_t)flag->width > flag->p)
-		w = (size_t)flag->width - flag->p;
-	else
-		w = 0;
-	// printf("w = %ld\n", w);
-
-	while (w > 0)
-	{
-		write(1, " ", 1);
-		++flag->written;
-		--w;
-	}
-
+	init(len, w, flag);
+	width_padder(w, ' ', flag);
 	flag->written += ft_putstrn(arg, flag->p);
 }
 
-// Undefined flags: #, 0, ' ', +
-// Applicable flags: -
-// Precision: maximum amount of characters
-void	output_str(char *arg, t_data *flag)
+/*
+** Undefined flags: #, 0, ' ', +
+** Applicable flags: -
+** Precision: maximum amount of characters
+*/
+
+void		output_str(char *arg, t_data *flag)
 {
 	if (flag->bit & FLAG_JUSTIFY_LEFT)
 		justify_left(arg, flag);
